fbfdd5fa592773edb3ce552450783093e03b083d
BetaNeutrino <sanir35@proton.me>
2025-09-17 23:00:35 +0600

    Basic Float point calculator
    new thing to collection

---
+++ Calculator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float choice,var1,var2,ans;
+    printf("What type of calculation you wanna do?\n");
+    printf("1. x+y(Addition)\n");
+    printf("2. x-y(Substract)\n");
+    printf("3. x*y(Multiply)\n");
+    printf("4. x/y(Divide)\n");
+    printf("5. x^1/2(Square Root)\n");
+    scanf("%f", &choice);
+    if (choice==1) {
+        printf("Put the numbers you want to addition: x y\n");
+        scanf("%f %f", &var1,&var2);
+        ans=var1+var2;
+        printf("The answer is %2.2f", ans);
+    }
+    else if (choice==2) {
+        printf("Put the numbers you want to substract: x y\n");
+        scanf("%f %f", &var1,&var2);
+        ans=var1-var2;
+        printf("The answer is %2.2f", ans);
+    }
+    else if (choice==3) {
+        printf("Put the numbers you want to multiply: x y\n");
+        scanf("%f %f", &var1,&var2);
+        ans=var1*var2;
+        printf("The answer is %2.2f", ans);
+    }
+    else if (choice==4) {
+        printf("Put the numbers you want to divide: x y\n");
+        scanf("%f %f", &var1,&var2);
+        ans=var1/var2;
+        printf("The answer is %2.2f", ans);
+    }
+    else if (choice==5) {
+        printf("Put the numbers you want to square root: x\n");
+        scanf("%f", &var1);
+        ans=sqrt(var1);
+        printf("The answer is %2.2f", ans);
+    }
+    else {
+        printf("Invalid choice.\n");
+    }
+}
